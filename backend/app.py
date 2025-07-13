6c7d816e91595c6dfb95923cba4ec39c797d6d7f
tashad19 <tashadurrahman1924@gmail.com>
2025-07-13 18:35:33 +0530

    backend config

--- backend/app.py
+++ backend/app.py
@@ -118,4 +118,5 @@ def analyze_excel():
 
 # === Run Server ===
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port, debug=True)
